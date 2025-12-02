1888c2532cb074ad6bc184cb65e254614127b551
Jason Wodicka <jason@wodicka.net>
2025-12-02 15:40:01 -0800

    Day 2 part 2!

--- day2/day2.py
+++ day2/day2.py
@@ -1,11 +1,11 @@
 """
 Find invalid values in ranges
 
-The ranges are separated by commas (,); each range gives its first ID and last ID 
+The ranges are separated by commas (,); each range gives its first ID and last ID
 separated by a dash (-).
 
-You can find the invalid IDs by looking for any ID which is made only of some 
-sequence of digits repeated twice. So, 55 (5 twice), 6464 (64 twice), and 
+You can find the invalid IDs by looking for any ID which is made only of some
+sequence of digits repeated twice. So, 55 (5 twice), 6464 (64 twice), and
 123123 (123 twice) would all be invalid IDs.
 """
 
@@ -14,9 +14,11 @@ import math
 
 echo = click.echo
 
+
 @click.group()
 def group():
     """Sure is a group"""
+
 
 @group.command()
 @click.argument("input", type=click.File("r"))
@@ -30,10 +32,29 @@ def part1(input):
     for low, high in ranges:
         for val in range(low, high + 1):
             # Check each value for legality
-                if is_illegal_id(val):
-                     bad_ids.append(val)
+            if is_illegal_id(val):
+                bad_ids.append(val)
     echo(sum(bad_ids))
-                     
+
+
+@group.command()
+@click.argument("input", type=click.File("r"))
+def part2(input):
+    """Solves part 2"""
+    # Parse input into range definitions
+    ranges = parse_ranges(input)
+
+    bad_ids = []
+    # Iterate through all values in ranges
+    for low, high in ranges:
+        for val in range(low, high + 1):
+            # Check each value for legality
+            if is_illegal_id_v2(val):
+                bad_ids.append(val)
+    echo(bad_ids)
+    
+    echo(sum(bad_ids))
+
 
 def parse_ranges(input):
     """
@@ -47,6 +68,7 @@ def parse_ranges(input):
         result.append((int(low), int(high)))
     return result
 
+
 def is_illegal_id(num):
     rep = str(num)
     length = len(rep)
@@ -55,6 +77,23 @@ def is_illegal_id(num):
     left = rep[:length//2]
     right = rep[length//2:]
     return left == right
+
+
+def is_illegal_id_v2(num):
+    rep = str(num)
+    start_size = len(rep)//2
+    if start_size < 1:
+        start_size = 1
+    for size in range(start_size, 0, -1):
+        if len(rep) % size != 0:
+            continue
+        chunks = [rep[idx:idx+size] for idx in range(0, len(rep), size)]
+        if len(chunks) == 1:
+            continue
+        if len(set(chunks)) == 1:
+            return True
+
+    return False
 
 
 # When invoked as a script, do this
