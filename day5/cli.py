93606e42001832e452d8368e72b5c127aea46531
Jason Wodicka <jason@wodicka.net>
2025-12-08 15:59:47 -0800

    Day 5 part 2

--- day5/cli.py
+++ day5/cli.py
@@ -40,6 +40,42 @@ def part1(input):
 def part2(input):
     """Solves part 2"""
 
+    ranges_s, _ = input.read().split("\n\n")
+    ranges_list = ranges_s.split("\n")
+    ranges_pairs = list(map(lambda s : s.split("-"), ranges_list))
+    ranges = list(map(lambda pair: (int(pair[0]), int(pair[1])), ranges_pairs))
+
+    # echo(ranges)
+
+    sorted_ranges = sorted(ranges, key=lambda r : r[0])
+    
+    echo("SORTED")
+    echo(sorted_ranges)
+
+    merged_ranges = []
+    prev_low = sorted_ranges[0][0]
+    prev_high = sorted_ranges[0][1]
+    for low, high in sorted_ranges:
+        if low <= prev_high:
+            if high > prev_high:
+                prev_high = high
+        else:
+            merged_ranges.append((prev_low, prev_high))
+            prev_low = low
+            prev_high = high
+    merged_ranges.append((prev_low, prev_high))
+
+    echo("MERGED")
+    echo(merged_ranges)
+
+    range_sizes = list(map(lambda range : range[1] - range[0] + 1, merged_ranges))
+
+    echo(range_sizes)
+
+    echo(sum(range_sizes))
+
+
+
 # When invoked as a script, do this
 if __name__ == '__main__':
     day5()
