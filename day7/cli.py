b4ba782089b817f0fd64afe4088b8b97139299c9
Jason Wodicka <jason@wodicka.net>
2025-12-08 17:03:21 -0800

    Day 7 part 2

--- day7/cli.py
+++ day7/cli.py
@@ -55,6 +55,40 @@ def part1(input):
 def part2(input):
     """Solves part 2"""
 
+    lines = input.read().splitlines()
+    width = len(lines[0])
+    
+    state = [0] * width
+    echo(state)
+    first_line = lines[0]
+    beam_start = first_line.index('S')
+    state[beam_start] = 1
+    echo(state)
+
+
+    def findall(source, target):
+        result = []
+        index = source.find(target)
+        while index > 0:
+            result.append(index)
+            index = source.find(target, index + 1)
+        return result
+    
+    next_state = state
+    for line in lines[1:]:
+        splitters = findall(line, '^')
+        if splitters:
+            for split in splitters:
+                if state[split] > 0:
+                    next_state[split-1] = next_state[split-1] + state[split]
+                    next_state[split+1] = next_state[split+1] + state[split]
+                    next_state[split] = 0
+        state = next_state
+        echo(state)
+
+    echo(state)
+    echo(sum(state))
+
 # When invoked as a script, do this
 if __name__ == '__main__':
     day7()
