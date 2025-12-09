c901f9d757bf25163c25b0e7839e2aa8f44eda2b
Jason Wodicka <jason@wodicka.net>
2025-12-08 16:40:42 -0800

    Day 6 part 2

--- day6/cli.py
+++ day6/cli.py
@@ -3,6 +3,7 @@ Problem definition here
 """
 
 import re
+import functools
 import click
 echo = click.echo
 
@@ -43,6 +44,59 @@ def part1(input):
 @click.argument("input", type=click.File("r"))
 def part2(input):
     """Solves part 2"""
+
+    lines = input.read().splitlines()
+    columns = list(zip(*lines, strict=True))
+
+    def strip_space(tuple):
+        return list(filter(lambda item : item != " ", tuple))
+
+    stripped = list(map(strip_space, columns))
+
+    problems = []
+    current_problem = []
+    current_operator = ""
+    for column in stripped:
+        match column:
+            case [*digits, "*"]:
+                current_operator = "*"
+                current_problem.append(int("".join(digits)))
+            case [*digits, "+"]:
+                current_operator = "+"
+                current_problem.append(int("".join(digits)))
+            case []:
+                problems.append((current_operator, current_problem))
+                current_problem = []
+                current_operator = ""
+            case [*digits]:
+                current_problem.append(int("".join(digits)))
+            case _:
+                echo(column)
+                raise ValueError("That's not math!")
+    problems.append((current_operator, current_problem))
+
+    echo(problems)
+
+    def mult(a, b):
+        return a * b
+
+    def solve(problem):
+        match problem:
+            case ('*', digits):
+                return functools.reduce(mult, digits)
+            case ('+', digits):
+                return sum(digits)
+            case _:
+                echo(problem)
+                raise ValueError("That's not math!")
+            
+    solutions = list(map(solve, problems))
+
+    echo(solutions)
+
+    echo(sum(solutions))
+        
+
 
 # When invoked as a script, do this
 if __name__ == '__main__':
